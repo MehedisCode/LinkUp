e59c59bc2895910de42583be3877c1f9c1746c07
Md Mehedi Hasan <mehedialways@gmail.com>
2024-05-04 07:32:49 +0600

    project 1 show

--- base/models.py
+++ base/models.py
@@ -19,9 +19,10 @@ class Room(models.Model):
         User, related_name='participants', blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
     class Meta:
         ordering = ['-updated', '-created']
-        
+
     def __str__(self):
         return self.name
 
